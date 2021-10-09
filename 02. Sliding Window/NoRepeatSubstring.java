6aa0e9125926524a274921fa2d357210a5fd25cc
Eric Chin <ericychin94@gmail.com>
2021-10-09 12:32:12 -0700

    Added template

---
+++ 02. Sliding Window/NoRepeatSubstring.java
@@ -0,0 +1,34 @@
+/*
+No Repeat Substring [HARD]
+
+Given a string, find the length of the longest substring which has no repeating characters.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          String = "aabccbb"
+Output:         3
+Explanation:    The longest substring without any repeating characters is "abc".
+
+- EX 2 -
+Input:          String = "abbbb"
+Output:         2
+Explanation:    The longest substring without any repeating characters is "ab".
+
+- EX 3 -
+Input:          String = "abccde"
+Output:         3
+Explanation:    The longestt substrings without any repeating characters are "abc" & "cde".
+*/
+
+public class NoRepeatSubstring {
+
+    public static int findLength(String str) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}

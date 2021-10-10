0353d9d7e2d247f54bc06d846ea92d009fa0c43b
Eric Chin <ericychin94@gmail.com>
2021-10-10 00:24:24 -0700

    Added template

---
+++ 02. Sliding Window/WordsConcatenation.java
@@ -0,0 +1,34 @@
+/*
+--- PROBLEM CHALLENGE 4 ---
+Words Concatenation [HARD]
+
+Given a string and a list of words, find all the starting indices of substrings in the
+given string that are a concatenation of all the given words exactly once without any
+overlapping of words. It is given that all words are of the same length.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          String = "catfoxcat", Words = ["cat", "fox"]
+Output:         [0, 3]
+Explanation:    The two substring containing both the words are "catfox" & "foxcat".
+
+- EX 2 -
+Input:          String = "catcatfoxfox", Words = ["cat", "fox"]
+Output:         [3]
+Explanation:    The only substring containing both the words is "catfox".
+*/
+
+import java.util.*;
+
+public class WordsConcatenation {
+    public static List<Integer> findWordConcatenation(String str, String[] words) {
+        List<Integer> resultIndices = new ArrayList<>();
+
+        return resultIndices;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

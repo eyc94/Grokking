1e0006648c639120e3774f8324bfa828b7eaa477
Eric Chin <ericychin94@gmail.com>
2021-10-09 22:42:58 -0700

    Added code and test cases

--- 02. Sliding Window/SmallestWindowContainingSubstring.java
+++ 02. Sliding Window/SmallestWindowContainingSubstring.java
@@ -23,12 +23,62 @@ Output:         ""
 Explanation:    No substring in the given string has all characters of the pattern.
 */
 
+import java.util.*;
+
 public class SmallestWindowContainingSubstring {
     public static String findSubstring(String str, String pattern) {
-        return "";
+        HashMap<Character, Integer> charFreq = new HashMap<>();
+        for (char letter : pattern.toCharArray()) {
+            charFreq.put(letter, charFreq.getOrDefault(letter, 0) + 1);
+        }
+
+        int subStringStart = 0;
+        int subStringEnd = 0;
+
+        int matched = 0;
+        int windowStart = 0;
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            if (charFreq.containsKey(rightChar)) {
+                charFreq.put(rightChar, charFreq.get(rightChar) - 1);
+                if (charFreq.get(rightChar) == 0) {
+                    matched++;
+                }
+            }
+
+            while (matched == charFreq.size()) {
+                subStringStart = windowStart;
+                subStringEnd = windowEnd;
+                char leftChar = str.charAt(windowStart);
+                if (charFreq.containsKey(leftChar)) {
+                    if (charFreq.get(leftChar) == 0) {
+                        matched--;
+                    }
+                    charFreq.put(leftChar, charFreq.get(leftChar) + 1);
+                }
+                windowStart++;
+            }
+        }
+        if (subStringStart == 0 && subStringEnd == 0) {
+            return "";
+        }
+        return str.substring(subStringStart, subStringEnd + 1);
     }
 
     public static void main(String[] args) {
+        // Sample strings.
+        String s1 = "aabdec";
+        String s2 = "abdabca";
+        String s3 = "adcad";
 
+        // Calculate results.
+        String r1 = findSubstring(s1, "abc");
+        String r2 = findSubstring(s2, "abc");
+        String r3 = findSubstring(s3, "abc");
+
+        // Print results.
+        System.out.println("The smallest substring of " + s1 + " with the pattern 'abc' is: " + r1);
+        System.out.println("The smallest substring of " + s2 + " with the pattern 'abc' is: " + r2);
+        System.out.println("The smallest substring of " + s3 + " with the pattern 'abc' is: " + r3);
     }
 }

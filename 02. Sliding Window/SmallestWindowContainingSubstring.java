9e22c55339028e3183d9dab1a088a0181496300b
Eric Chin <ericychin94@gmail.com>
2021-10-09 22:00:12 -0700

    Added template

---
+++ 02. Sliding Window/SmallestWindowContainingSubstring.java
@@ -0,0 +1,34 @@
+/*
+--- PROBLEM CHALLENGE 3 ---
+Smallest Window Containing Substring [HARD]
+
+Given a string and a pattern, find the smallest substring in the given string which
+has all the characters of the given pattern.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          String = "aabdec", Pattern = "abc"
+Output:         "abdec"
+Explanation:    The smallest substring having all characters of the pattern is "abdec".
+
+- EX 2 -
+Input:          String = "abdabca", Pattern = "abc"
+Output:         "abc"
+Explanation:    The smallest substring having all characters of the pattern is "abc".
+
+- EX 3 -
+Input:          String = "abcad", Pattern = "abc"
+Output:         ""
+Explanation:    No substring in the given string has all characters of the pattern.
+*/
+
+public class SmallestWindowContainingSubstring {
+    public static String findSubstring(String str, String pattern) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

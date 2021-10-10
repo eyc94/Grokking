fac0d47a816bdac1b2efa945ba4441a1f4666ca6
Eric Chin <ericychin94@gmail.com>
2021-10-09 21:20:01 -0700

    Added template

---
+++ 02. Sliding Window/StringAnagrams.java
@@ -0,0 +1,46 @@
+/*
+--- PROBLEM CHALLENGE 2 ---
+String Anagrams [HARD]
+
+Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+Anagram is actually a Permutation of a string. For example, "abc" has the following size
+anagrams:
+
+    1. abc
+    2. acb
+    3. bac
+    4. bca
+    5. cab
+    6. cba
+
+Write a function to return a list of starting indices of the anagrams of the pattern in
+the given string.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          String = "ppqp", Pattern = "pq"
+Output:         [1, 2]
+Explanation:    The two anagrams of the pattern in the given string are "pq" and "qp".
+
+- EX 2 -
+Input:          String = "abbcabc", Pattern = "abc"
+Output:         [2, 3, 4]
+Explanation:    The three anagrams of the pattern in the given string are "bca", "cab",
+                and "abc".
+*/
+
+import java.util.*;
+
+public class StringAnagrams {
+    public static List<Integer> findStringAnagrams(String str, String pattern) {
+        List<Integer> resultIndices = new ArrayList<>();
+
+        return resultIndices;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

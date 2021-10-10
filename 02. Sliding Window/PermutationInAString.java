b7cc83efa54a11c15c2251eb82eb7bb16caf493c
Eric Chin <ericychin94@gmail.com>
2021-10-09 19:30:27 -0700

    Added template

---
+++ 02. Sliding Window/PermutationInAString.java
@@ -0,0 +1,51 @@
+/*
+--- PROBLEM CHALLENGE 1 ---
+Permutation In A String [HARD]
+
+Given a string and a pattern, find out if the string contains any permutation of the pattern.
+
+Permutation is defined as the re-arranging of the characters of the string. For example, "abc"
+has the following six permutations:
+
+    1. abc
+    2. acb
+    3. bac
+    4. bca
+    5. cab
+    6. cba
+
+If a string has 'n' distinct characters it will have n! permutations.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          String = "oidbcaf", Pattern = "abc"
+Output:         true
+Explanation:    The string contains "bca" which is a permutation of the given pattern.
+
+- EX 2 -
+Input:          String = "odicf", Pattern = "dc"
+Output:         false
+Explanation:    No permutation of the pattern is present in the given string as a substring.
+
+- EX 3 -
+Input:          String = "bcdxabcdy", Pattern = "bcdyabcdx"
+Output:         true
+Explanation:    Both the string and the pattern are a permutation of each other.
+
+- EX 4 -
+Input:          String = "aaacb", Pattern = "abc"
+Output:         true
+Explanation:    The string contains "acb" which is a permutation of the given pattern.
+*/
+
+public class PermutationInAString {
+
+    public static boolean findPermutation(String str, String pattern) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

c0af48634f8f86c22912c1665e7762b76f7ea2bb
Eric Chin <ericychin94@gmail.com>
2021-10-10 02:58:07 -0700

    Added code

--- 02. Sliding Window/WordsConcatenation.java
+++ 02. Sliding Window/WordsConcatenation.java
@@ -23,12 +23,56 @@ import java.util.*;
 
 public class WordsConcatenation {
     public static List<Integer> findWordConcatenation(String str, String[] words) {
+        HashMap<String, Integer> wordFreq = new HashMap<>();
+        for (String word : words) {
+            wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
+        }
+
         List<Integer> resultIndices = new ArrayList<>();
+        int wordsCount = words.length;
+        int wordLength = words[0].length();
+
+        for (int i = 0; i <= str.length() - wordsCount * wordLength; i++) {
+            HashMap<String, Integer> wordsSeen = new HashMap<>();
+            for (int j = 0; j < wordsCount; j++) {
+                int nextWordIndex = i + j * wordLength;
+
+                String word = str.substring(nextWordIndex, nextWordIndex + wordLength);
+                if (!wordFreq.containsKey(word)) {
+                    break;
+                }
+
+                wordsSeen.put(word, wordsSeen.getOrDefault(word, 0) + 1);
+
+                if (wordsSeen.get(word) > wordFreq.getOrDefault(word, 0)) {
+                    break;
+                }
+
+                if (j + 1 == wordsCount) {
+                    resultIndices.add(i);
+                }
+            }
+        }
 
         return resultIndices;
     }
 
     public static void main(String[] args) {
+        // Sample string.
+        String s1 = "catfoxcat";
+        String s2 = "catcatfoxfox";
 
+        // Sample words.
+        String[] w1 = new String[] { "cat", "fox" };
+
+        // Calculate results.
+        List<Integer> r1 = findWordConcatenation(s1, w1);
+        List<Integer> r2 = findWordConcatenation(s2, w1);
+
+        // Print results.
+        System.out.println(
+                "The starting indices of " + s1 + " that contain the words in " + Arrays.toString(w1) + " is " + r1);
+        System.out.println(
+                "The starting indices of " + s2 + " that contain the words in " + Arrays.toString(w1) + " is " + r2);
     }
 }

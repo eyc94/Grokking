d5a1561570c4f53c4953fab25cf3852a3e033bf0
Eric Chin <ericychin94@gmail.com>
2021-10-11 02:49:24 -0700

    Added comments and explanation

--- 03. Two Pointers/RemoveDuplicates.java
+++ 03. Two Pointers/RemoveDuplicates.java
@@ -21,9 +21,44 @@ import java.util.*;
 
 public class RemoveDuplicates {
 
+    /*
+     * This is the two pointer approach. We start both pointers from the start of
+     * our array. One pointer 'i' is the scanner. The other pointer
+     * 'nextNonDuplicate' is the border between those that are all unique (left) and
+     * those that we have not processed yet or are duplicates (right).
+     * 
+     * What we do is we scan from left to right. Compare the current 'i' value to
+     * the value before 'nextNonDuplicate'. If the values are the same, this means
+     * we have already placed this value in our left side. So, move on. If they are
+     * NOT the same, we swap the current 'i' value with the border
+     * (nextNonDuplicate) value.
+     * 
+     * This process repeats and the border will land on the index after the final
+     * number of the new array. Because it is 0-indexed, we can just return the
+     * border value (index) to the user.
+     * 
+     * Time Complexity: O(N) where N is the length of our array.
+     * 
+     * Space Complexity: O(1).
+     */
+
     public static int remove(int[] arr) {
+        // This is the pointer that tracks the next position in the array that we can
+        // swap. Everything before this pointer has no duplicates.
+        // We initialize it to 1 because the first number is itself not a duplicate of
+        // anything before it.
         int nextNonDuplicate = 1;
+        // We initialize our iterator to 1. This is the same location as the next non
+        // duplicate. We iterate through to the end of the array.
         for (int i = 1; i < arr.length; i++) {
+            // Compare the iterator value to the value before the next non duplicate.
+            // If the values are equal, we DO NOT need to swap. This is because the value
+            // placed in the unique list to the left of nextNonDuplicate already has the
+            // number we're looking at. So we DO NOT need to swap.
+
+            // HOWEVER. If the current iterator value IS NOT equal to the value before the
+            // border, we can move the current iterator value to the border value because we
+            // have not seen the value yet. Then, increment the border (nextNonDuplicate).
             if (arr[i] != arr[nextNonDuplicate - 1]) {
                 arr[nextNonDuplicate] = arr[i];
                 nextNonDuplicate++;

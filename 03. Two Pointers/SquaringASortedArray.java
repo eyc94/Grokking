459b4b2e287a36d9efd8faf9149195490c0dde56
Eric Chin <ericychin94@gmail.com>
2021-10-11 16:34:15 -0700

    Added template

---
+++ 03. Two Pointers/SquaringASortedArray.java
@@ -0,0 +1,28 @@
+/*
+Squaring A Sorted Array [EASY]
+
+Given a sorted array, create a new array containing squares of all the numbers of the
+input array in the sorted order.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          [-2, -1, 0, 2, 3]
+Output:         [0, 1, 4, 4, 9]
+
+- EX 2 -
+Input:          [-3, -1, 0, 1, 2]
+Output:         [0, 1, 1, 4, 9]
+*/
+
+public class SquaringASortedArray {
+    public static int[] makeSquares(int[] arr) {
+        int[] squares = new int[arr.length];
+
+        return squares;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

36988481f8c99bfd918bb3004a3dcd42542b923f
Eric Chin <ericychin94@gmail.com>
2021-10-11 21:16:25 -0700

    Added template

---
+++ 03. Two Pointers/TripletSumCloseToTarget.java
@@ -0,0 +1,34 @@
+/*
+Triplet Sum Close To Target [MEDIUM]
+
+Given an array of unsorted numbers and a target number, find a triplet in the array whose
+sum is as close to the target number as possible. Return the sum of the triplet. If there
+are more than one such triplet, return the sum of the triplet with the smallest sum.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          [-2, 0, 1, 2], target = 2
+Output:         1
+Explanation:    The triplet [-2, 1, 2] has the closest sum to the target.
+
+- EX 2 -
+Input:          [-3, -1, 1, 2], target = 1
+Output:         0
+Explanation:    The triplet [-3, 1, 2] has the closest sum to the target.
+
+- EX 3 -
+Input:          [1, 0, 1, 1], target = 100
+Output:         3
+Explanation:    The triplet [1, 1, 1] has the closest sum to the target.
+*/
+
+public class TripletSumCloseToTarget {
+    public static int searchTriplet(int[] arr, int targetSum) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

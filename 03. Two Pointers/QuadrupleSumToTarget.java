421d90d70d682cc72bc5ee5ff2649b9a65ad7597
Eric Chin <ericychin94@gmail.com>
2021-10-12 15:48:20 -0700

    Added template

---
+++ 03. Two Pointers/QuadrupleSumToTarget.java
@@ -0,0 +1,33 @@
+/*
+--- PROBLEM CHALLENGE 1 ---
+Quadruple Sum To Target [MEDIUM]
+
+Given an array of unsorted numbers and a target number, find all unique quadruplets in it,
+whose sum is equal to the target number.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          [4, 1, 2, -1, 1, -3], target = 1
+Output:         [-3, -1, 1, 4], [-3, 1, 1, 2]
+Explanation:    Both the quadruplets add up to the target.
+
+- EX 2 -
+Input:          [2, 0, -1, 1, -2, 2], target = 2
+Output:         [-2, 0, 2, 2], [-1, 0, 1, 2]
+Explanation:    Both the quadruplets add up to the target.
+*/
+
+import java.util.*;
+
+public class QuadrupleSumToTarget {
+    public static List<List<Integer>> searchQuadruplets(int[] arr, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<>();
+
+        return quadruplets;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

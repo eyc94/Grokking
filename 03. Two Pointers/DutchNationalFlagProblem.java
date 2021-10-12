881a21184d4524352d80f538fea8f4e18d20ad47
Eric Chin <ericychin94@gmail.com>
2021-10-12 12:57:32 -0700

    Added template

---
+++ 03. Two Pointers/DutchNationalFlagProblem.java
@@ -0,0 +1,31 @@
+/*
+Dutch National Flag Problem [MEDIUM]
+
+Given an array containing 0s, 1s, and 2s, sort the array in-place. You should treat numbers of
+the array as objects, hence, we can't count 0s, 1s, and 2s to recreate the array.
+
+The flag of the Netherlands consists of three colors: red, white, and blue; and since our input
+array also consists of three different numbers that is why it is called "Dutch National Flag
+Problem".
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          [1, 0, 2, 1, 0]
+Output:         [0, 0, 1, 1, 2]
+
+- EX 2 -
+Input:          [2, 2, 0, 1, 2, 0]
+Output:         [0, 0, 1, 2, 2, 2]
+*/
+
+public class DutchNationalFlagProblem {
+
+    public static void sort(int[] arr) {
+        return;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

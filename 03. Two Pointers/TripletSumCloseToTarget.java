57693b924dd2f899c3b0101ad1239d0880462606
Eric Chin <ericychin94@gmail.com>
2021-10-11 21:48:29 -0700

    Added code and tests

--- 03. Two Pointers/TripletSumCloseToTarget.java
+++ 03. Two Pointers/TripletSumCloseToTarget.java
@@ -23,12 +23,54 @@ Output:         3
 Explanation:    The triplet [1, 1, 1] has the closest sum to the target.
 */
 
+import java.util.*;
+
 public class TripletSumCloseToTarget {
     public static int searchTriplet(int[] arr, int targetSum) {
-        return -1;
+        Arrays.sort(arr);
+
+        int closestDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length - 2; i++) {
+            int low = i + 1;
+            int high = arr.length - 1;
+
+            while (low < high) {
+                int distance = targetSum - arr[i] - arr[low] - arr[high];
+
+                if (distance == 0) {
+                    return targetSum - distance;
+                }
+
+                if (Math.abs(distance) < Math.abs(closestDistance)) {
+                    closestDistance = distance;
+                }
+
+                if (distance > 0) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
+
+        }
+
+        return targetSum - closestDistance;
     }
 
     public static void main(String[] args) {
+        // Sample arrays.
+        int[] s1 = new int[] { -2, 0, 1, 2 };
+        int[] s2 = new int[] { -3, -1, 1, 2 };
+        int[] s3 = new int[] { 1, 0, 1, 1 };
 
+        // Calculate results.
+        int r1 = searchTriplet(s1, 2);
+        int r2 = searchTriplet(s2, 1);
+        int r3 = searchTriplet(s3, 100);
+
+        // Print results.
+        System.out.println("The sum of the triplet that sums closests to 2 in " + Arrays.toString(s1) + " is: " + r1);
+        System.out.println("The sum of the triplet that sums closests to 1 in " + Arrays.toString(s2) + " is: " + r2);
+        System.out.println("The sum of the triplet that sums closests to 100 in " + Arrays.toString(s3) + " is: " + r3);
     }
 }

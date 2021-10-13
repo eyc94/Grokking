1b50c7711cf652ffdd4dbeddf6675bb18e566f15
Eric Chin <ericychin94@gmail.com>
2021-10-12 19:39:13 -0700

    Added comments and explanation

--- 03. Two Pointers/MinimumWindowSort.java
+++ 03. Two Pointers/MinimumWindowSort.java
@@ -30,37 +30,84 @@ Explanation:    The whole array needs to be sorted.
 import java.util.*;
 
 public class MinimumWindowSort {
+
+    /*
+     * This is the two pointer approach. We put the two pointers on both ends. This
+     * is a little easy and tricky.
+     * 
+     * We move our low pointer up until (low + 1)'s value is less than low's value.
+     * The low pointer now points to the start of our window.
+     * 
+     * We move our high pointer down until (high - 1)'s value is greater than high's
+     * value. The high pointer now points to the end of the window.
+     * 
+     * We find the maximum and minimum values in our current window. This is because
+     * we still might have values outside our window that needs to go inside our
+     * window. For example, our minimum value of window can be -1. We can still have
+     * the value of 1 outside our window as well.
+     * 
+     * We move our low pointer down while our (low - 1)'s value is greater than the
+     * minimum of our window. Our low pointer now points to the proper location.
+     * 
+     * We move our high pointer up while our (high + 1)'s value is less than the
+     * maximum of our window. Our high pointer now points to the proper location.
+     * 
+     * We now return the length of the window.
+     * 
+     * Time Complexity: O(N) where N is the length of our input array.
+     * 
+     * Space Complexity: O(1).
+     */
+
     public static int sort(int[] arr) {
+        // This is the two pointers starting from both ends.
+        // Represents the borders of the subarray that we need to sort.
         int low = 0;
         int high = arr.length - 1;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
 
+        // Move low up until we find a number that is less than the current iteration
+        // number. This denotes a discrepancy in the sorted array. The 'low' pointer now
+        // points to the start of the window.
         while (low < arr.length - 1 && arr[low] <= arr[low + 1]) {
             low++;
         }
 
+        // If low is equal to the last element of the array, this means the array is
+        // already sorted. So, return 0.
         if (low == arr.length - 1) {
             return 0;
         }
 
+        // Move high down until we find a number that is greater than the current
+        // iteration number. This denotes a discrepancy. The 'high' pointer now points
+        // to the start of the discrepancy.
         while (high > 0 && arr[high] >= arr[high - 1]) {
             high--;
         }
 
+        // Find the min and max of the window.
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = low; i <= high; i++) {
             min = Math.min(min, arr[i]);
             max = Math.max(max, arr[i]);
         }
 
+        // We need to extend low pointer down more in case there are other numbers that
+        // are greater than the minimum value of our window.
+        // The low pointer now points to the proper spot.
         while (low > 0 && arr[low - 1] > min) {
             low--;
         }
 
+        // We need to extend our high pointer up more in case there are other numbers
+        // that are less than the maximum value of our window.
+        // The high pointer now points to the proper spot.
         while (high < arr.length - 1 && arr[high + 1] < max) {
             high++;
         }
 
+        // Return the length of our window.
         return high - low + 1;
     }
 

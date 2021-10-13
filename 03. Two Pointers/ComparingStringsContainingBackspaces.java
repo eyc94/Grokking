48f2dd626202f314ac05e7b2e92431820833632a
Eric Chin <ericychin94@gmail.com>
2021-10-12 18:27:04 -0700

    Added my code

--- 03. Two Pointers/ComparingStringsContainingBackspaces.java
+++ 03. Two Pointers/ComparingStringsContainingBackspaces.java
@@ -31,10 +31,63 @@ Explanation:    After applying backspaces the strings become "xywrrmp" and "xywr
 
 public class ComparingStringsContainingBackspaces {
     public static boolean compare(String str1, String str2) {
-        return false;
+        StringBuilder stringOne = new StringBuilder();
+        StringBuilder stringTwo = new StringBuilder();
+        int backspaceCount = 0;
+
+        for (int i = str1.length() - 1; i >= 0;) {
+            if (str1.charAt(i) == '#') {
+                backspaceCount++;
+                i--;
+            } else {
+                if (backspaceCount > 0 && i > 0) {
+                    backspaceCount--;
+                } else {
+                    stringOne.append(str1.charAt(i));
+                }
+                i--;
+            }
+        }
+
+        backspaceCount = 0;
+        for (int i = str2.length() - 1; i >= 0;) {
+            if (str2.charAt(i) == '#') {
+                backspaceCount++;
+                i--;
+            } else {
+                if (backspaceCount > 0 && i > 0) {
+                    backspaceCount--;
+                } else {
+                    stringTwo.append(str2.charAt(i));
+                }
+                i--;
+            }
+        }
+
+        return stringOne.toString().equals(stringTwo.toString());
     }
 
     public static void main(String[] args) {
+        // Strings.
+        String s1 = "xy#z";
+        String s2 = "xzz#";
+        String s3 = "xy#z";
+        String s4 = "xyz#";
+        String s5 = "xp#";
+        String s6 = "xyz##";
+        String s7 = "xywrrmp";
+        String s8 = "xywrrmu#p";
 
+        // Results.
+        boolean r1 = compare(s1, s2);
+        boolean r2 = compare(s3, s4);
+        boolean r3 = compare(s5, s6);
+        boolean r4 = compare(s7, s8);
+
+        // Print.
+        System.out.println("After backspaces, the strings " + s1 + " and " + s2 + " are equal: " + r1);
+        System.out.println("After backspaces, the strings " + s3 + " and " + s4 + " are equal: " + r2);
+        System.out.println("After backspaces, the strings " + s5 + " and " + s6 + " are equal: " + r3);
+        System.out.println("After backspaces, the strings " + s7 + " and " + s8 + " are equal: " + r4);
     }
 }

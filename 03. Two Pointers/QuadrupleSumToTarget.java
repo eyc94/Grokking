7a4bef893450c1b2c24c0f057965c40ea97b3525
Eric Chin <ericychin94@gmail.com>
2021-10-12 17:43:50 -0700

    Added code

--- 03. Two Pointers/QuadrupleSumToTarget.java
+++ 03. Two Pointers/QuadrupleSumToTarget.java
@@ -22,12 +22,57 @@ import java.util.*;
 
 public class QuadrupleSumToTarget {
     public static List<List<Integer>> searchQuadruplets(int[] arr, int target) {
+        Arrays.sort(arr);
         List<List<Integer>> quadruplets = new ArrayList<>();
+
+        for (int i = 0; i < arr.length - 3; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < arr.length - 2; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) {
+                    continue;
+                }
+                int low = j + 1;
+                int high = arr.length - 1;
+
+                while (low < high) {
+                    int quadrupletSum = arr[i] + arr[j] + arr[low] + arr[high];
+                    if (quadrupletSum == target) {
+                        quadruplets.add(Arrays.asList(arr[i], arr[j], arr[low], arr[high]));
+                        low++;
+                        high--;
+
+                        while (low < high && arr[low] == arr[low - 1]) {
+                            low++;
+                        }
+
+                        while (low < high && arr[high] == arr[high + 1]) {
+                            high--;
+                        }
+                    } else if (quadrupletSum < target) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+        }
 
         return quadruplets;
     }
 
     public static void main(String[] args) {
+        // Arrays.
+        int[] s1 = new int[] { 4, 1, 2, -1, 1, -3 };
+        int[] s2 = new int[] { 2, 0, -1, 1, -2, 2 };
 
+        // Results.
+        List<List<Integer>> r1 = searchQuadruplets(s1, 1);
+        List<List<Integer>> r2 = searchQuadruplets(s2, 2);
+
+        // Print.
+        System.out.println("The quadruplets of " + Arrays.toString(s1) + " that add to 1 are: " + r1);
+        System.out.println("The quadruplets of " + Arrays.toString(s2) + " that add to 2 are: " + r2);
     }
 }

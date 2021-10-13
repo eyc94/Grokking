d60d30d182227dedf952e3837cd60b0045a579c1
Eric Chin <ericychin94@gmail.com>
2021-10-12 19:08:58 -0700

    Added template

---
+++ 03. Two Pointers/MinimumWindowSort.java
@@ -0,0 +1,38 @@
+/*
+--- PROBLEM CHALLENGE 3 ---
+
+Given an array, find the length of the smallest subarray in it which when sorted will
+sort the whole array.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          [1, 2, 5, 3, 7, 10, 9, 12]
+Output:         5
+Explanation:    We need to sort only the subarray [5, 3, 7, 10, 9] to make the whole array sorted.
+
+- EX 2 -
+Input:          [1, 3, 2, 0, -1, 7, 10]
+Output:         5
+Explanation:    We need to sort only the subarray [1, 3, 2, 0, -1] to make the whole array sorted.
+
+- EX 3 -
+Input:          [1, 2, 3]
+Output:         0
+Explanation:    The array is already sorted.
+
+- EX 4 -
+Input:          [3, 2, 1]
+Output:         3
+Explanation:    The whole array needs to be sorted.
+*/
+
+public class MinimumWindowSort {
+    public static int sort(int[] arr) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

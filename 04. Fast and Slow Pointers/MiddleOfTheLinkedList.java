7b720af48b6a8d1cf1c1c76ca7c66dda180a3c8d
Eric Chin <ericychin94@gmail.com>
2021-10-14 02:50:50 -0700

    Added code

--- 04. Fast and Slow Pointers/MiddleOfTheLinkedList.java
+++ 04. Fast and Slow Pointers/MiddleOfTheLinkedList.java
@@ -35,8 +35,15 @@ public class MiddleOfTheLinkedList {
     }
 
     public static ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
 
-        return head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 
     public static void main(String[] args) {

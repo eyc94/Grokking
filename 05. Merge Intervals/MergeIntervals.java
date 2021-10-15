1e6d30fea293377f4bab52f27f50bd96a4575066
Eric Chin <ericychin94@gmail.com>
2021-10-15 02:23:38 -0700

    Added explanation

--- 05. Merge Intervals/MergeIntervals.java
+++ 05. Merge Intervals/MergeIntervals.java
@@ -26,6 +26,74 @@ import java.util.*;
 
 public class MergeIntervals {
 
+    /*
+     * This is a pretty easy and fun problem to solve. We create a class called
+     * Interval that stores the start and end values.
+     * 
+     * First, check if our intervals list is less than size 2. If so, we know there
+     * is only 1 or 0 intervals in our list. This means that we can just return the
+     * intervals list. Merged interval of 1 interval is just that interval. Merged
+     * interval of 0 is just 0.
+     * 
+     * Our plan is to iterate through the start times and check if each start time
+     * is <= to the previous end time. That is, we want to check if there is any
+     * overlap in the intervals.
+     * 
+     * So, we must sort by start time. Because we have a collection of Intervals, we
+     * can use the following:
+     * 
+     * Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+     * 
+     * 1. Collections.sort() takes in the collections to sort and a comparator to
+     * denote criteria to compare.
+     * 
+     * 2. We use the lambda expression '(para1, para2) -> expression'. This takes in
+     * two parameters and returns whatever the expression outputs.
+     * 
+     * 3. Integer.compare(x, y) returns 0 if x == y, -1 if x < y, and 1 if x > y.
+     * 
+     * We create a list of Intervals to return to the user. This stores merged
+     * intervals.
+     * 
+     * We want to start with the first interval. We create an Iterator object from
+     * the Interval collection. We use the .iterator() method to create an Iterator
+     * object from our collection. Use the .next() method to get the interval. Grab
+     * the start and end values of that interval.
+     * 
+     * Use the while loop to iterate through our collection with the .hasNext()
+     * method. This iterates while we have something in our collection. Grab the
+     * next interval with .next() function.
+     * 
+     * Grab our current interval's start time. Check if it is less than previous
+     * interval's end time. If it is, we update the end time. This is because we
+     * know that our current start time falls inside the previous interval. This is
+     * overlapping. We just need to know if our current end time is greater or
+     * smaller than the previous end time. We take the max of the previous end and
+     * current end.
+     * 
+     * If the current start time is > than the previous end time, we know that our
+     * current interval is NOT in the previous interval. So, we can go ahead and add
+     * the previous interval that we have so far (start and end values) to the
+     * merged list. We then update the start and end values to take on the current
+     * interval's start and end values.
+     * 
+     * Once our while loop ends, we will have 'start' and 'end' values leftover.
+     * From the first 'if' statement, we do not add anything to the merged list.
+     * From the 'else' statement, we just add our previous interval NOT our current
+     * interval. If our loop ended there, our current interval will NOT be accounted
+     * for. This leaves a leftover either way. So, we add the 'start' and 'end' as a
+     * new interval to the merged list.
+     * 
+     * Return the merged list.
+     * 
+     * Time Complexity: O(N log N) where N is the size of the list of Intervals. It
+     * takes O(N log N) to sort the intervals. It takes O(N) to process the
+     * intervals. Total is O(N + N log N). This is O(N log N) asymptotically.
+     * 
+     * Space Complexity: O(N) because we need to return list of merged intervals. We
+     * need O(N) for sorting. Total is O(N).
+     */
+
     // This is the Interval class.
     public static class Interval {
         int start;

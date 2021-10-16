f64dd7dd150f461c57449d2414d30abdc5b04338
Eric Chin <ericychin94@gmail.com>
2021-10-16 14:46:17 -0700

    Added code and edited tests

--- 05. Merge Intervals/IntervalsIntersection.java
+++ 05. Merge Intervals/IntervalsIntersection.java
@@ -33,6 +33,23 @@ public class IntervalsIntersection {
 
     public static Interval[] merge(Interval[] arr1, Interval[] arr2) {
         List<Interval> intervalsIntersection = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if ((arr1[i].start >= arr2[j].start && arr1[i].start <= arr2[j].end)
+                    || (arr2[j].start >= arr1[i].start && arr2[j].start <= arr1[i].end)) {
+                intervalsIntersection
+                        .add(new Interval(Math.max(arr1[i].start, arr2[j].start), Math.min(arr1[i].end, arr2[j].end)));
+            }
+
+            if (arr1[i].end < arr2[j].end) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
         return intervalsIntersection.toArray(new Interval[intervalsIntersection.size()]);
     }
 
@@ -40,21 +57,42 @@ public class IntervalsIntersection {
         // Sample 1.
         Interval[] s1 = new Interval[] { new Interval(1, 3), new Interval(5, 6), new Interval(7, 9) };
         Interval[] s2 = new Interval[] { new Interval(2, 3), new Interval(5, 7) };
+        System.out.println("---------------------------------------------------");
+        System.out.println("Original Lists: ");
+        for (Interval interval : s1) {
+            System.out.print("[" + interval.start + ", " + interval.end + "] ");
+        }
+        System.out.println();
+        for (Interval interval : s2) {
+            System.out.print("[" + interval.start + ", " + interval.end + "] ");
+        }
+        System.out.println();
         Interval[] result = merge(s1, s2);
         System.out.print("Intervals Intersection: ");
         for (Interval interval : result) {
             System.out.print("[" + interval.start + ", " + interval.end + "] ");
         }
         System.out.println();
+        System.out.println("---------------------------------------------------");
 
         // Sample 2.
         s1 = new Interval[] { new Interval(1, 3), new Interval(5, 7), new Interval(9, 12) };
         s2 = new Interval[] { new Interval(5, 10) };
+        System.out.println("Original Lists: ");
+        for (Interval interval : s1) {
+            System.out.print("[" + interval.start + ", " + interval.end + "] ");
+        }
+        System.out.println();
+        for (Interval interval : s2) {
+            System.out.print("[" + interval.start + ", " + interval.end + "] ");
+        }
+        System.out.println();
         result = merge(s1, s2);
         System.out.print("Intervals Intersection: ");
         for (Interval interval : result) {
             System.out.print("[" + interval.start + ", " + interval.end + "] ");
         }
         System.out.println();
+        System.out.println("---------------------------------------------------");
     }
 }

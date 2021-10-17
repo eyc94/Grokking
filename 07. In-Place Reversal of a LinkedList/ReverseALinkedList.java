b096e42f8f51e2dc089c9877795571961ddef161
Eric Chin <ericychin94@gmail.com>
2021-10-17 13:58:40 -0700

    Changed tests and added code

--- 07. In-Place Reversal of a LinkedList/ReverseALinkedList.java
+++ 07. In-Place Reversal of a LinkedList/ReverseALinkedList.java
@@ -24,21 +24,51 @@ public class ReverseALinkedList {
     }
 
     public static ListNode reverse(ListNode head) {
-        return head;
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
     }
 
     public static void main(String[] args) {
+        // Sample Linked List.
         ListNode head = new ListNode(2);
         head.next = new ListNode(4);
         head.next.next = new ListNode(6);
         head.next.next.next = new ListNode(8);
         head.next.next.next.next = new ListNode(10);
 
+        // Print original LinkedList.
+        ListNode scanner = head;
+        System.out.println("Nodes of the original LinkedList are: ");
+        while (scanner != null) {
+            if (scanner.next != null) {
+                System.out.print(scanner.value + " -> ");
+            } else {
+                System.out.print(scanner.value);
+            }
+            scanner = scanner.next;
+        }
+        System.out.println();
+
+        // Reverse and print reversed LinkedList.
         ListNode result = reverse(head);
         System.out.println("Nodes of the reversed LinkedList are: ");
         while (result != null) {
-            System.out.print(result.value + " ");
+            if (result.next != null) {
+                System.out.print(result.value + " -> ");
+            } else {
+                System.out.print(result.value);
+            }
             result = result.next;
         }
+        System.out.println();
     }
 }

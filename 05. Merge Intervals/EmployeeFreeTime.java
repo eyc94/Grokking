5e245374edfdadc703914263dba28f4ef7af9639
Eric Chin <ericychin94@gmail.com>
2021-10-17 00:43:02 -0700

    Added code and comments

--- 05. Merge Intervals/EmployeeFreeTime.java
+++ 05. Merge Intervals/EmployeeFreeTime.java
@@ -53,10 +53,44 @@ public class EmployeeFreeTime {
 
     public static List<Interval> findEmployeeFreeTime(List<List<Interval>> schedule) {
         List<Interval> result = new ArrayList<>();
+
+        // Create a Min Heap that is sorted by ascending start time.
+        PriorityQueue<EmployeeInterval> minHeap = new PriorityQueue<>(
+                (a, b) -> Integer.compare(a.interval.start, b.interval.start));
+
+        // Insert first interval of each employee to the Min Heap.
+        for (int i = 0; i < schedule.size(); i++) {
+            minHeap.offer(new EmployeeInterval(schedule.get(i).get(0), i, 0));
+        }
+
+        Interval previousInterval = minHeap.peek().interval;
+        while (!minHeap.isEmpty()) {
+            EmployeeInterval queueTop = minHeap.poll();
+            // If previousInterval is not overlapping with the next interval, insert a free
+            // interval.
+            if (previousInterval.end < queueTop.interval.start) {
+                result.add(new Interval(previousInterval.end, queueTop.interval.start));
+                previousInterval = queueTop.interval;
+            } else { // Overlapping intervals, update the previous interval if needed.
+                if (previousInterval.end < queueTop.interval.end) {
+                    previousInterval = queueTop.interval;
+                }
+            }
+
+            // If there are more intervals available for the same employee, add their next
+            // interval.
+            List<Interval> employeeSchedule = schedule.get(queueTop.employeeIndex);
+            if (employeeSchedule.size() > queueTop.intervalIndex + 1) {
+                minHeap.offer(new EmployeeInterval(employeeSchedule.get(queueTop.intervalIndex + 1),
+                        queueTop.employeeIndex, queueTop.intervalIndex + 1));
+            }
+        }
+
         return result;
     }
 
     public static void main(String[] args) {
+        // Sample 1.
         List<List<Interval>> s1 = new ArrayList<>();
         s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(5, 6))));
         s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 3), new Interval(6, 8))));
@@ -67,6 +101,7 @@ public class EmployeeFreeTime {
         }
         System.out.println();
 
+        // Sample 2.
         s1 = new ArrayList<>();
         s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(9, 12))));
         s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
@@ -78,10 +113,11 @@ public class EmployeeFreeTime {
         }
         System.out.println();
 
+        // Sample 3.
         s1 = new ArrayList<>();
         s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3))));
         s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
-        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(6, 8), new Interval(7, 9))));
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(3, 5), new Interval(7, 9))));
         result = findEmployeeFreeTime(s1);
         System.out.println("Free Intervals: ");
         for (Interval interval : result) {

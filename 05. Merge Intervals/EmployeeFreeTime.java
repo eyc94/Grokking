c31a4409d48a92a51150c5be5776140e6f9b5c9c
Eric Chin <ericychin94@gmail.com>
2021-10-17 00:22:31 -0700

    Added template

---
+++ 05. Merge Intervals/EmployeeFreeTime.java
@@ -0,0 +1,92 @@
+/*
+--- PROBLEM CHALLENGE 3 ---
+
+For 'K' employees, we are given a list of intervals representing the working hours of each
+employee. Our goal is to find out if there is a free interval that is common to all employees.
+You can assume that each list of employee working hours is sorted on the start time.
+
+--- EXAMPLES ---
+
+- EX 1 -
+Input:          Employee Working Hours = [[[1, 3], [5, 6]], [[2, 3], [6, 8]]]
+Output:         [3, 5]
+Explanation:    Both the employees are free between [3, 5]
+
+- EX 2 -
+Input:          Employee Working Hours = [[[1, 3], [9, 12]], [[2, 4]], [[6, 8]]]
+Output:         [4, 6], [8, 9]
+Explanation:    All employees are free between [4, 6] and [8, 9].
+
+- EX 3 -
+Input:          Employee Working Hours = [[[1, 3]], [[2, 4]], [[3, 5], [7, 9]]]
+Output:         [5, 7]
+Explanation:    All employees are free between [5, 7].
+*/
+
+import java.util.*;
+
+public class EmployeeFreeTime {
+
+    // This is the Interval class.
+    public static class Interval {
+        int start;
+        int end;
+
+        public Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    // This is the EmployeeInterval class.
+    public static class EmployeeInterval {
+        Interval interval; // Interval representing employee's working hours.
+        int employeeIndex; // Index of the list containing working hours of this employee.
+        int intervalIndex; // Index of the interval in the employee list.
+
+        public EmployeeInterval(Interval interval, int employeeIndex, int intervalIndex) {
+            this.interval = interval;
+            this.employeeIndex = employeeIndex;
+            this.intervalIndex = intervalIndex;
+        }
+    }
+
+    public static List<Interval> findEmployeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> result = new ArrayList<>();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<List<Interval>> s1 = new ArrayList<>();
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(5, 6))));
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 3), new Interval(6, 8))));
+        List<Interval> result = findEmployeeFreeTime(s1);
+        System.out.println("Free Intervals: ");
+        for (Interval interval : result) {
+            System.out.print("[" + interval.start + ", " + interval.end + "]");
+        }
+        System.out.println();
+
+        s1 = new ArrayList<>();
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3), new Interval(9, 12))));
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(6, 8))));
+        result = findEmployeeFreeTime(s1);
+        System.out.println("Free Intervals: ");
+        for (Interval interval : result) {
+            System.out.print("[" + interval.start + ", " + interval.end + "]");
+        }
+        System.out.println();
+
+        s1 = new ArrayList<>();
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(1, 3))));
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(2, 4))));
+        s1.add(new ArrayList<Interval>(Arrays.asList(new Interval(6, 8), new Interval(7, 9))));
+        result = findEmployeeFreeTime(s1);
+        System.out.println("Free Intervals: ");
+        for (Interval interval : result) {
+            System.out.print("[" + interval.start + ", " + interval.end + "]");
+        }
+        System.out.println();
+    }
+}

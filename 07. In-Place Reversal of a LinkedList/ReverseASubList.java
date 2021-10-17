4cb0d955b2b1abee52db85319ffe69a133f5db1b
Eric Chin <ericychin94@gmail.com>
2021-10-17 15:25:05 -0700

    Added explanation

--- 07. In-Place Reversal of a LinkedList/ReverseASubList.java
+++ 07. In-Place Reversal of a LinkedList/ReverseASubList.java
@@ -13,6 +13,48 @@ Explanation:    We reverse the nodes from positions 2 to 4 (index 1 to index 3).
 */
 
 public class ReverseASubList {
+
+    /*
+     * This is the Reversed LinkedList problem but we need to tweak it a bit.
+     * Basically, we just reverse the sublist given by the inputs 'p' and 'q'.
+     * Notice how the values are not 0-index based. This means we have to offset the
+     * end of loops to the left by 1.
+     * 
+     * We need a few things to solve this problem. We need a reference to node 'p',
+     * node 'p - 1' (to connect to the head of the new reversed sub-list 'prev'),
+     * node 'q + 1' to connect the new tail of our reversed sub-list to.
+     * 
+     * We first skip over the first 'p - 1' nodes. We keep track of the previous and
+     * current pointers. At the end of this iteration, curr will point to the start
+     * of the sub-list (node 'p') and prev will point to the node before 'p'
+     * (position 'p - 1'). NOTE: If 'p' is 1, this denotes the first node, and prev
+     * will remain null.
+     * 
+     * After this, we found 'p' and 'p - 1'. So, we should keep a reference to these
+     * nodes because we'll be using prev and curr to reverse the sub-list. So we
+     * create a reference to both of these to connect the reversed sub-list to the
+     * original list.
+     * 
+     * Reverse the sub-list like normal from position 'p' to position 'q'. At the
+     * end, prev will point to the new head of our reversed sub-list. The curr
+     * pointer will point to 'q + 1'.
+     * 
+     * Now, we check to see if the reference to 'p - 1' is null or not. If it is
+     * null, this means 'p' has a value of 1, and there is no previous node before
+     * 'p'. This means that prev is the new head. So, point head to prev.
+     * 
+     * If 'p - 1' is NOT null, we know there is a node before 'p'. So, point the
+     * next pointer of 'p - 1' to point to prev.
+     * 
+     * Lastly, we need to point our reversed sub-list tail to 'q + 1'. We have the
+     * reference to that tail a position 'p'. Point the next pointer of 'p' to 'q +
+     * 1' which is referenced by curr.
+     * 
+     * Time Complexity: O(N) where N is the length of our LinkedList.
+     * 
+     * Space Complexity: O(1).
+     */
+
     // This is the ListNode class.
     public static class ListNode {
         int value = 0;

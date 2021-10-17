a37b9ea144b3b5a30851af33399aab674b92c202
Eric Chin <ericychin94@gmail.com>
2021-10-17 02:56:24 -0700

    Added explanation

--- 05. Merge Intervals/EmployeeFreeTime.java
+++ 05. Merge Intervals/EmployeeFreeTime.java
@@ -27,6 +27,57 @@ import java.util.*;
 
 public class EmployeeFreeTime {
 
+    /*
+     * This code is fairly straightforward. The only thing to look out for is that
+     * we need to keep track of which employee the interval belongs to. So, we make
+     * another class called EmployeeInterval. This holds the interval, the employee
+     * index, the interval index in the List of the employee's schedule.
+     * 
+     * So, we create a Min Heap that is ordered by ascending interval start times.
+     * It will hold EmployeeIntervals.
+     * 
+     * We add the first intervals of every employee into the Min Heap as an
+     * EmployeeInterval object.
+     * 
+     * The idea is to continuously take EmployeeIntervals out and compare it to the
+     * previously seen EmployeeInterval. So, remove the first EmployeeInterval in
+     * Min Heap and store in a pointer indicating current interval.
+     * 
+     * If the previous end time is < the current interval's start time, we know
+     * there is no overlap. We can create a free interval and add to results. The
+     * free interval is the previous's end and current's start time. Update the
+     * previousInterval to point to current interval for next iteration.
+     * 
+     * If the previous end time is >= the current interval's start time, we know
+     * there is an overlap. We don't really do much because there is no free time
+     * here. What we want is to set up previousInterval for the next iteration. If
+     * the previous interval's end time is >= than the current interval's end time,
+     * we keep the previous pointer the same. This is because the previous interval
+     * has a chance of overlapping with the next iteration's interval.
+     * 
+     * If the previous interval's end time is < the current interval's end time, we
+     * want to use the current interval as the previousInterval for the next
+     * iteration. So, update it.
+     * 
+     * Lastly, we want to add the next interval the employee worked for the employee
+     * that we removed at the beginning. To do this, we grab the List containing the
+     * Intervals of that employee. (This is where using EmployeeInterval's
+     * properties is good).
+     * 
+     * Once we get the list, we check to see if the interval's index reached the end
+     * of the list. (That is, if there is more intervals to add). If there is, we
+     * create a new EmployeeInterval object and place it in the Min Heap. Remember
+     * to increment the intervalIndex property.
+     * 
+     * Once the Min Heap is empty, the while loop ends. Return the results.
+     * 
+     * Time Complexity: O(N log K) where N is the number of intervals and K is the
+     * number of employees. This is because we iterate through all N intervals. On
+     * each N intervals, we do remove or add employees which takes O(log K) time.
+     * 
+     * Space Complexity: O(K). Min Heap will not have more than 'K' elements.
+     */
+
     // This is the Interval class.
     public static class Interval {
         int start;

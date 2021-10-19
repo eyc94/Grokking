ca2be76280b8d8dfedac2f60b716f4475af83233
Eric Chin <ericychin94@gmail.com>
2021-10-18 21:57:13 -0700

    Added explanation

--- 09. Tree Depth First Search/SumOfPathNumbers.java
+++ 09. Tree Depth First Search/SumOfPathNumbers.java
@@ -28,6 +28,18 @@ Explanation:    The sum of all path numbers: 101 + 116 + 115 = 332.
 */
 
 public class SumOfPathNumbers {
+
+    /*
+     * The idea of recursing here is the same. The difference is that when we
+     * return, we add to the result of the right and left traversals. This is kind
+     * of hard to grasp at first.
+     * 
+     * Time Complexity: O(N) where N is the number of nodes in our tree. We traverse
+     * the nodes once.
+     * 
+     * Space Complexity: O(N) because of the recursion call stack.
+     */
+
     // This is the TreeNode class.
     public static class TreeNode {
         int val;
@@ -40,20 +52,29 @@ public class SumOfPathNumbers {
     }
 
     public static int findSumOfPathNumbers(TreeNode root) {
+        // Call the dfs helper with the initial sum of 0.
         return findRootToLeafPathNumbers(root, 0);
     }
 
     public static int findRootToLeafPathNumbers(TreeNode currentNode, int pathSum) {
+        // If the node is null, return 0.
+        // This only happens when the node is the child of a leaf and when the node is a
+        // child of a node with one child (other child).
         if (currentNode == null) {
             return 0;
         }
 
+        // Update the path sum.
+        // We multiply our path sum by 10 to leave room for the next digit to be tacked
+        // on.
         pathSum = 10 * pathSum + currentNode.val;
 
+        // If we encounter a leaf, return the path sum.
         if (currentNode.left == null && currentNode.right == null) {
             return pathSum;
         }
 
+        // Traverse the left and right children with the current path sum.
         return findRootToLeafPathNumbers(currentNode.left, pathSum)
                 + findRootToLeafPathNumbers(currentNode.right, pathSum);
     }

6027cbe5974ebd85d65474ad3d1e172dbaaac1eb
Eric Chin <ericychin94@gmail.com>
2021-10-20 04:05:02 -0700

    Added recursive solution

--- 11. Subsets/Permutations.java
+++ 11. Subsets/Permutations.java
@@ -132,6 +132,27 @@ public class Permutations {
         return result;
     }
 
+    // This is the recursive solution.
+    public static List<List<Integer>> generatePermutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generatePermutationsRecursive(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    public static void generatePermutationsRecursive(int[] nums, int index, List<Integer> currentPermutation,
+            List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(currentPermutation);
+        } else {
+            // Create a new permutation by adding current number at every position.
+            for (int i = 0; i <= currentPermutation.size(); i++) {
+                List<Integer> newPermutation = new ArrayList<>(currentPermutation);
+                newPermutation.add(i, nums[index]);
+                generatePermutationsRecursive(nums, index + 1, newPermutation, result);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         List<List<Integer>> result = findPermutations(new int[] { 1, 3, 5 });
         System.out.println("Here are all the permutations: " + result);

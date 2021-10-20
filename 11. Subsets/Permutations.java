58db8a0998759e940024f431493c1e24dfccb03b
Eric Chin <ericychin94@gmail.com>
2021-10-20 03:38:01 -0700

    Added code

--- 11. Subsets/Permutations.java
+++ 11. Subsets/Permutations.java
@@ -27,6 +27,24 @@ import java.util.*;
 public class Permutations {
     public static List<List<Integer>> findPermutations(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> permutations = new LinkedList<>();
+        permutations.add(new ArrayList<>());
+
+        for (int currentNumber : nums) {
+            int n = permutations.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> oldPermutation = permutations.poll();
+                for (int j = 0; j <= oldPermutation.size(); j++) {
+                    List<Integer> newPermutation = new ArrayList<>(oldPermutation);
+                    newPermutation.add(j, currentNumber);
+                    if (newPermutation.size() == nums.length) {
+                        result.add(newPermutation);
+                    } else {
+                        permutations.add(newPermutation);
+                    }
+                }
+            }
+        }
 
         return result;
     }
